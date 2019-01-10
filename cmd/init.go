739e11704dd4e00b6a9c1993cb003aabd2b1f10a
Jay R Bolton <jayrbolton@gmail.com>
2019-01-10 12:08:46 -0800

    Dont generate compile report; the entrypoint.sh report command will do it from within the docker container

--- cmd/init.go
+++ cmd/init.go
@@ -57,8 +57,6 @@ var initCmd = &cobra.Command{
     write_template(module_name + "/.gitignore", gitignore, &empty_map)
     // Write README.md
     write_template(module_name + "/README.md", readme, &module_info)
-    // Write compile_report.json
-    write_template(module_name + "/compile_report.json", compile_report, &module_info)
     // We're done.
     log.Printf("Your new module lives in ./%v\n", module_name)
     log.Printf("Get started with: cd %v && kbase-sdk test\n", module_name)
@@ -165,6 +163,7 @@ ENTRYPOINT ["sh", "/usr/local/bin/entrypoint.sh"]  # from the kbase_module packa
 var gitignore = `
 # KBase build files
 compile_report.json
+/work
 
 # Byte-compiled / optimized / DLL files
 __pycache__/
@@ -211,14 +210,3 @@ This is a [KBase](https://kbase.us) module. You need to install the SDK command-
 1. Install the [KBase SDK](https://github.com/jayrbolton/kbase_sdk_cli)
 2. Run tests with: kbase-sdk test
 `
-
-var compile_report = `{
-  "function_places": {},
-  "functions": {},
-  "impl_file_path": "",
-  "module_name": "{{.Name}}",
-  "sdk_git_commit": "0",
-  "sdk_version": "0",
-  "spec_files": [{"content": "", "is_main": 1}]
-}
-` // end compile_report
